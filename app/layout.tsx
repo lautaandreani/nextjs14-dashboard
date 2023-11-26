099114268ca57a704e7d3c7015c796e51af3b29b
lautaro.andreani <lautaroandreani2016@gmail.com>
2023-11-26 16:26:07 -0300

    feat: first commit nextjs 14 course

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,17 @@
+import { Metadata } from 'next';
+
+import '@/app/ui/global.css';
+import { nunito } from './ui/fonts/fonts';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${nunito.className} antialised`}>{children}</body>
     </html>
   );
 }
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,28 @@
 'use client';
-
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useDebouncedCallback } from 'use-debounce';
+const WAIT_BETWEEN_CHANGES = 300;
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const value = e.target.value;
+      const params = new URLSearchParams(searchParams);
+
+      if (value) params.set('query', value);
+      else params.delete('query');
+      params.set('page', '1');
+
+      replace(`${pathname}?${params.toString()}`);
+    },
+    WAIT_BETWEEN_CHANGES,
+  );
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +31,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        autoFocus
+        onChange={handleSearch}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
